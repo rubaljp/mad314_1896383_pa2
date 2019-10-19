e77d541795b3e05b55a46d4aa4ada52dd880ece3
rubaljp <jatinder174@gmail.com>
2019-10-19 12:34:54 -0400

    final commit

--- app/src/main/java/com/example/assignment2/Main2Activity.java
+++ app/src/main/java/com/example/assignment2/Main2Activity.java
@@ -34,6 +34,58 @@ public class Main2Activity extends AppCompatActivity {
         editID=findViewById(R.id.editID);
         editPW=findViewById(R.id.editPw);
 
+
+        String name1 ="rahul" ;
+        int id1 = 2;
+
+
+
+
+         Student student1 = new Student();
+        student1.name = name1;
+        student1.id = id1;
+
+
+        // add animal to repo
+        repository.addStudent(student1);
+
+        mAdapter.update();
+        String name2 ="gres" ;
+        int id2 = 3;
+
+
+
+
+        Student student2 = new Student();
+        student2.name = name2;
+        student2.id = id2;
+
+
+        // add animal to repo
+        repository.addStudent(student2);
+
+        mAdapter.update();
+
+        //4th
+        String name3 ="fghfghg" ;
+        int id3 = 4;
+        Student student3 = new Student();
+        student3.name = name3;
+        student3.id = id3;
+
+        //5th
+        String name4 ="fggjgfhg" ;
+        int id4 = 5;
+        Student student4 = new Student();
+        student4.name = name4;
+        student4.id = id4;
+
+
+        // add animal to repo
+        repository.addStudent(student3);
+
+        mAdapter.update();
+        //
         btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
